3650a4342cc4dd17286ffc84543ff46fd1aebe16
Peter Elliott <p.elliott332@gmail.com>
2017-02-06 17:43:18 -0700

    updaed drawing for web sockets

---
+++ draw.js
@@ -0,0 +1,41 @@
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return [
+        evt.clientX - rect.left,
+        evt.clientY - rect.top
+    ];
+}
+
+
+function Drawable(canvas, socket) {
+    var ctx = canvas.getContext('2d');
+
+    var isdown = false;
+    var last;
+
+    canvas.addEventListener("mousedown", function(e) {
+        last = getMousePos(canvas, e);
+        isdown = true;
+    });
+
+    canvas.addEventListener("mouseup", function(e) {
+        socket.send("-");
+        isdown = false;
+    });
+
+    canvas.addEventListener("mousemove", function(e) {
+        if (isdown) {
+            var current = getMousePos(canvas, e);
+
+            socket.send(current[0]+"|"+current[1]);
+
+            ctx.beginPath();
+            ctx.moveTo(...last);
+            ctx.lineTo(...current);
+            ctx.stroke();
+
+            last = current;
+        }
+    });
+}
